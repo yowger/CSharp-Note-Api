0fe11a0dfcbe98fa7f33c7ad14c55d28068e4bfd
Ver0018 <rogerpantilyowger@gmail.com>
2025-02-13 00:51:27 +0800

    add initial dependency

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapScalarApiReference();
+    app.MapOpenApi();
+}
+
+// app.MapGet("/", () => "GET");
+
+// app.MapPost("/", () => "POST");
+
+// app.MapPut("/", () => "PUT");
+
+// app.MapDelete("/", () => "DELETE");
 
 app.Run();
